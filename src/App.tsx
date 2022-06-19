4e63c893d4c5dc84c8547c2b8eba2d97adb530ce
Kth <vcro12123@naver.com>
2022-06-19 16:11:46 +0900

    fc 타입 수정하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Todos from './components/Todos';
 
-function App() {
+const App = () => {
+  const [items, setItems] = useState(['Learn React,', 'Learn TypeScript']);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Todos items={items} />
     </div>
   );
-}
+};
 
 export default App;
